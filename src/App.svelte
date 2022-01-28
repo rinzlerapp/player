d0743838d55d90ab67cb8fa12d8a21f2b2016ed2
Furkan Demir <frndemir@gmail.com>
2022-01-28 20:05:23 +0300

    :tada: begin project

---
+++ src/App.svelte
@@ -0,0 +1,18 @@
+<script>
+  export let name;
+</script>
+
+<style>
+  .player {
+    position: fixed;
+    left: 0;
+    bottom: 0;
+    height: 80px;
+    width: 100vw;
+    border: 3px dashed #2b8a3e;
+  }
+</style>
+
+<div class="player">
+  {name} is mounted!
+</div>
